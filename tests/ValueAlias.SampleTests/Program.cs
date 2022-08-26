4e41aff5cbb355877271310571bc571416178a7c
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-26 18:51:53 +0700

    init project

---
+++ tests/ValueAlias.SampleTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using ValueAlias.SampleTests;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Host.CreateDefaultBuilder()
+    .UseSerilog()
+    .Build();
+
+Sampling.Run();
---
+++ tests/ValueAlias.SampleTests/Sampling.cs
@@ -0,0 +1,9 @@
+namespace ValueAlias.SampleTests;
+
+public static class Sampling
+{
+    public static void Run()
+    {
+        Serilog.Log.Information("Starting...");
+    }
+}
